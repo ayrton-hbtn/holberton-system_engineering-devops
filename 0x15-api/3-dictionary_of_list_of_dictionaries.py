6fcf7a1f3925cb5e944678b7108dd9ba257ee654
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-01-31 16:25:12 -0300

    all employees and their tasks

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""using the JSONPlaceholder API"""
+
+
+if __name__ == "__main__":
+    import requests as req
+    from sys import argv
+    import json
+    todos = req.get('https://jsonplaceholder.typicode.com/todos').json()
+    users = req.get('https://jsonplaceholder.typicode.com/users').json()
+    todo_all_users = {}
+    for user in users:
+        user_todos = []
+        for task in todos:
+            try:
+                if task['userId'] == user['id']:
+                    del task['userId']
+                    del task['id']
+                    task['username'] = user['username']
+                    task['task'] = task.pop('title')
+                    user_todos.append(task)
+            except Exception:
+                pass
+        todo_all_users[user['id']] = user_todos
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(todo_all_users, f)
