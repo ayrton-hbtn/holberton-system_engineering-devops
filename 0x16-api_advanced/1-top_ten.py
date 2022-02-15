40937de7f697d187274449d255b44efa2139d0b3
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-02-15 13:25:27 -0300

    top 10 from sub

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import requests as req
+
+
+def top_ten(subreddit):
+    """queries the titles of the first 10 hot posts
+    listed for a given subreddit"""
+    res = req.get(f'https://reddit.com/r/{subreddit}/top/.json?limit=10',
+                  headers={'User-Agent': 'Pepocho'}).json()
+    try:
+        for post in res['data']['children']:
+            print(post['data']['title'])
+    except KeyError:
+        print('None')
