3fb655119147e447bef655f2ea44f622959a9aad
ayrton-hbtn <3282@holbertonschool.com>
2022-02-17 12:16:30 -0300

    sorteddd gosh

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """100-count"""
 import requests as req
+import collections
 
 
 def count_words(subreddit, word_list, after='', word_count={}):
@@ -17,15 +18,17 @@ def count_words(subreddit, word_list, after='', word_count={}):
         title = post['data']['title'].lower().split(' ')
         for word in word_list:
             for title_word in title:
-                if word.lower() in title_word:
+                if word.lower() == title_word:
                     if word.lower() in word_count.keys():
                         word_count[word.lower()] += 1
                     else:
                         word_count[word.lower()] = 1
     if not after:
+        sorted_tup = sorted(word_count.items(), key=lambda item: item[1],
+                            reverse=True)
+        word_count = collections.OrderedDict(sorted_tup)
         word_list.sort()
-        for word in set(word_list):
-            if word in word_count.keys():
-                print('{}: {}'.format(word, word_count[word]))
+        for word in word_count.keys():
+            print('{}: {}'.format(word, word_count[word]))
         return
     return count_words(subreddit, word_list, after, word_count)
